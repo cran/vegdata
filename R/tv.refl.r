f359737590a07ecfdd581c01510fd9fb3bd4fe32
Florian Jansen <jansen@uni-greifswald.de>
2010-12-30 00:00:00 +0000

    version 0.5.3

--- R/tv.refl.r
+++ R/tv.refl.r
@@ -4,10 +4,10 @@ tv.refl <- function(db, refl, tv_home) {
 #       cap <- function(s) paste(toupper(substring(s,1,1)), {s <- substring(s,2); if(strict) toupper(s) else s}, sep = "", collapse = " " )
 #       sapply(strsplit(s, split = " "), cap, USE.NAMES = !is.null(names(s)))
 #   }
-  fun <- function(tmprefl) {
-    try(refl <- match.arg(tmprefl,list.files(file.path(tv_home,'Species')) ), silent=TRUE)
-    if(exists('refl')) refl
-  }
+#   fun <- function(tmprefl) {
+#     try(refl <- match.arg(tmprefl,list.files(file.path(tv_home,'Species')) ), silent=TRUE)
+#     if(exists('refl')) refl
+#   }
 
   if(missing(tv_home)) tv_home <- tv.home()
   if(!missing(db)) {
@@ -15,7 +15,7 @@ tv.refl <- function(db, refl, tv_home) {
       if(file.access(dbattr)==0) refl <-  sub('A\002', '', readBin(dbattr,what='character', n=3)[3]) else 
     stop('Database attribute file tvwin.set from database "', db, '" not available. Please specify name of taxonomic reference list!') 
   } else  refl <- 'GermanSL 1.2'
-  if(!exists(refl)) refl <- fun(gsub(' ','',refl))
+#  if(!exists(refl)) refl <- fun(gsub(' ','',refl))
   if(tolower(substr(refl, 1,8)) == 'germansl') refl <- paste('GermanSL', substring(refl,9,nchar(refl)), sep='')
   return(refl)
  }
--- R/tv.write.r
+++ R/tv.write.r
@@ -5,24 +5,33 @@
 # obs <- tv.coverperc(db)
 # site <- tv.site(db)
 
-tv.write <- function(obs, site, name, cover=c('code','perc'), overwrite = FALSE, ...) {
+tv.write <- function(x, site, name, cover=c('code','perc'), overwrite = FALSE, ...) {
+warning('This function is highly experimental and will most probably not work as expected.')
   cover <- match.arg(cover)
-  if(!any(c('tv.obs','vw.obs') %in% class(obs))) stop('Species observations must be either \"tv.obs\" or \"vw.obs\" class.')
-  if(!all(c('SPECIES_NR','RELEVE_NR','COVER_CODE') %in% names(obs))) 
-    stop('column names of species observations must contain SPECIES_NR, RELEVE_NR, COVER_CODE')
+  if('veg' %in% class(x)) {
+    x <- reShape.veg(x, ...)
+    cover <- 'perc'
+#   names(x)[names(x)=='COVER_PERC'] <- 'COVER_CODE'
+# print(names(x))
+    if(cover == 'perc') {
+      x$COVER_CODE <- as.character(x$COVER_PERC)
+      site$COVERSCALE <- '00'
+      x <- x[,c('RELEVE_NR','SPECIES_NR', 'COVER_CODE', 'LAYER')]
+    }
+  }
+
+if(!any(c('tv.obs','vw.obs') %in% class(x))) stop('Species observations must be of either \"tv.obs\" or \"vw.obs\" class.')
+  if(!all(c('RELEVE_NR') %in% names(x))) 
+    stop('column names of species observations must contain RELEVE_NR')
 
   if(!overwrite) if(file.exists(file.path(options('tv_home'), 'Data', name))) stop('Database ', name, ' already exists.')
   site$DATE <- gsub('-','',site$DATE)
-  if(cover == 'perc') {
-      obs$COVER_CODE <- as.integer(ceiling(obs$COVER_PERC))
-      site$COVERSCALE <- '00'
-  }
+#  site$RELEVE_NR <- as.integer(as.character(site$RELEVE_NR))
   dir.create(file.path(options('tv_home'), 'Data', name), showWarnings = TRUE)
   write.dbf(site, file.path(options('tv_home'), 'Data', name, 'tvhabita.dbf'))
-  write.dbf(obs, file.path(options('tv_home'), 'Data', name, 'tvabund.dbf'))
+  write.dbf(x, file.path(options('tv_home'), 'Data', name, 'tvabund.dbf'))
   file.copy(from=file.path(options('tv_home'), 'Data/taxatest/remarks.dbf'), to=file.path(options('tv_home'),'Data', name, 'remarks.dbf'), overwrite = overwrite)
-  cat('\n Please specify correct species list when first opening the database in Turboveg.\n',
-      ' If Turboveg is not able to open the database, try to exclude the biggest text fields.\n')
+  cat('Turboveg database', name, 'written to', unlist(options('tv_home')), '\n Please specify correct species list when first opening the database in Turboveg.\n', 'If Turboveg is not able to open the database, try to exclude the biggest text fields.\n')
 }
 
 # tv.write(obs, site, 'test2', overwrite=TRUE)
